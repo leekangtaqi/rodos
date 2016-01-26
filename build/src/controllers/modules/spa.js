72831eef0abe3ae9032c4a3a49a1857cc668653e
Rick Lee <328297079@qq.com>
2016-01-26 22:11:54 +0800

    Delete spa.js

--- build/src/controllers/modules/spa.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-function index() {
-    router.get('/', (req, res, next) => {
-        res.render('index', { username: 'zhangsan11' });
-    });
-    return router;
-}
-exports.index = index;
